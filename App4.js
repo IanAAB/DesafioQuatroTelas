ff9eb760b612e449fe458141c1e1e6c0d9c5e8f5
IanSC <99413314+IanAAB@users.noreply.github.com>
2023-04-27 12:02:27 -0300

    Update App4.js

---
+++ App4.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Linking
+} from "react-native";
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <View style={styles.textos}>
+      <Text style={styles.infinitamente}>Infinitamente grátis</Text>
+      <Text style={styles.planoInfinito}>Plano Infinito</Text>
+      <Text>R$ Grátis</Text>
+      <View style={styles.vantagens}>
+      <Text style={styles.um}>✔  Ouça músicas sem anúncios </Text>
+      <Text style={styles.dois}>✔  Ouça em qualquer lugar — até quando estiver offline</Text>
+      <Text style={styles.tres}>✔  Ouça músicas na ordem que quiser</Text>
+      <Text style={styles.quatro}>✔  Faça um plano pré-pago ou uma assinatura</Text>
+      </View> 
+      <View style={{ borderBottomWidth: 1, borderBottomColor: 'black' }} />
+      <Text style={styles.termos}>Somente no plano Individual. Depois, é só R$ 19,90/mês. Sujeito a Termos    e      Condições. Disponível apenas para quem nunca usou o Premium. A oferta termina em 16/05/2023.</Text>
+    </View>
+    <button type="button" style={styles.botao}>COMECE AGORA</button>
+      </View> 
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#efefef",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textos: {
+    margin: 30,
+    backgroundColor: "white",
+    padding: 25,
+    borderRadius: 4
+  },
+  infinitamente: {
+    backgroundColor: "#1DB954",
+    color: "#fffeff", 
+    borderRadius: 5,
+    padding: 5.5
+  },
+  planoInfinito: {
+    fontSize: 22,
+    fontWeight: "bold"
+  }, 
+  um: {
+    margin: 10
+  },
+  dois: {
+    margin: 10
+  },
+  tres: {
+    margin: 10
+  },
+  quatro: {
+    margin: 10
+  },
+  termos: {
+    fontSize: 10, 
+    margin: 5, 
+    width: 220
+  }, 
+  botao: {
+    backgroundColor: "#191414",
+    color: "white",
+    padding: 15, 
+    borderRadius: 500, 
+    border: 1,
+    width: 280
+  }
+});
